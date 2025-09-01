5c1929877c5830c3f7ac6651c2520f866cf82b3b
israeliiii <israelite1804@gmail.com>
2025-09-01 02:29:54 -0700

    add: question_6 solved

---
+++ answer_6.py
@@ -0,0 +1,10 @@
+"""   
+ Task 6:
+    Write a function that converts a temperature from Celsius to Fahrenheit.
+    Formula: (celsius * 9/5) + 32
+"""
+def converts_temperature(celsius):
+    return (celsius * 9/5) + 32
+
+
+print(converts_temperature(20))
