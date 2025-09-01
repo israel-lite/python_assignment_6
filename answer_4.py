d5b1f6c7e8a2e83e2d24e12ed21d4579459b088a
israeliiii <israelite1804@gmail.com>
2025-09-01 02:14:42 -0700

    add:question_4 solved

---
+++ answer_4.py
@@ -0,0 +1,15 @@
+"""
+ Task 4:
+    Write a function that accepts the length and width of a rectangle
+    and returns its area.
+    Formula: area = length * width
+ """
+
+
+def length_rectangle(length, width):
+    area = length * width
+    return area
+
+
+result = length_rectangle(5, 6)
+print(result)
