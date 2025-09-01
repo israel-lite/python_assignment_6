8a0036cf38bbb7fe8375314f7bf205934dd8150c
israeliiii <israelite1804@gmail.com>
2025-09-01 03:41:18 -0700

    add: question_11 solved

---
+++ answer_11.py
@@ -0,0 +1,11 @@
+"""
+    Task 11:
+    Write a function that accepts a string as a parameter
+    and returns the string reversed.
+    Example: reverse_string("hello") → "olleh"
+"""
+
+def reverse_string(text):
+    return text
+word = "hello"
+print(word[::-1])
