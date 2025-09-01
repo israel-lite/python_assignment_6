2d5c3dc17aa757831f1ed33aeb1617d066727f0e
israeliiii <israelite1804@gmail.com>
2025-09-01 02:23:28 -0700

    add:question_5 solved

---
+++ answer_5.py
@@ -0,0 +1,10 @@
+"""
+    Task 5:
+    Write a function that accepts the side length of a square
+    and returns its perimeter.
+    Formula: perimeter = 4 * side
+"""
+def perimeter_of_square(side):
+    return 4 * side
+
+print(perimeter_of_square(5))
