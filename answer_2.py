dede58af4a986d5cf92af6c6dd2370769f02b2aa
israeliiii <israelite1804@gmail.com>
2025-09-01 01:52:59 -0700

    tasks_2 solved

---
+++ answer_2.py
@@ -0,0 +1,12 @@
+"""
+  Task 2:
+    Write a function that accepts a number and returns its square.
+    Example: square_number(5) → 25
+ """
+
+def square_number(n):
+    return n * n
+
+
+result = square_number(5)
+print(result)
